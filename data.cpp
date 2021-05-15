d61c68a959e380f2ad6ecf29031c11e97c0cbea3
Debian <debian@debian10.linuxvmimages.local>
2021-05-15 14:45:26 -0500

    data

---
+++ data.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int data()
+{
+	FILE *fp;
+	char data[10];
+	fp = fopen("data.txt", "r");
+	if ( fp == NULL)
+	{
+		printf("Could not open file data.txt");
+		return 1;
+	}
+	
+	int *ptr;
+	int sum = 0;
+
+
+	ptr = (int*) malloc(100 * sizeof(int));
+	if(ptr == NULL)
+	{
+		printf("Error! memory not allocated. ");
+		exit(0);
+	}
+
+	while ( fscanf(fp, "%d", *ptr)!= EOF)
+	{
+		fscanf(fp, "%d", *ptr);
+		sum = sum +  *ptr;
+	}
+
+	printf("Sum = %d", sum);
+
+	free(ptr);
+	fclose(fp);
+	
+	return sum;
+
+}
